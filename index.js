1e016d16c4873cc6f8e2d8b31a97c9291089abca
sajal90 <sajal2005gupta@gmail.com>
2025-03-29 18:39:57 +0530

    phonbookback 10

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(express.static("dist"));
 
 morgan.token("data", (request, response) => {
 	if (request.method === "POST") {
@@ -102,7 +101,7 @@ app.post("/api/persons", (request, response) => {
 	response.status(201).json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 
 console.log("app running on port", PORT);
