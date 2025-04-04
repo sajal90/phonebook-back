815857aaa7434a9b506fa67b66929923ed9234b3
sajal90 <sajal2005gupta@gmail.com>
2025-04-04 23:57:40 +0530

    3.20

--- models/person.js
+++ models/person.js
@@ -17,7 +17,16 @@ const personSchema = new mongoose.Schema({
 		type: String,
 		minLength: 3,
 	},
-	number: String,
+	number: {
+		type: String,
+		minLength: 8,
+		validate: {
+			validator: (v) => {
+				return /\d{2,3}-\d+/.test(v);
+			},
+			message: (props) => `${props.value} is not a valid phone number`,
+		},
+	},
 });
 
 personSchema.set("toJSON", {
